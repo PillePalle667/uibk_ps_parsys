51018459bd21f5d1f3f55ce1b72815ebe2045fe6
Behrbe <bernhard.behr@student.uibk.ac.at>
2018-10-16 13:56:28 +0200

    Adding Script

--- exercise01/mmul.cpp
+++ exercise01/mmul.cpp
@@ -30,8 +30,34 @@ Matrix operator*(const Matrix& a, const Matrix& b) {
 	}
 	return c;
 }
+/**
+ *
+ * Matrix operator*(const Matrix& a, const Matrix& b) {
+	unsigned n = a.size();
+	Matrix c = id(n);
+	for(unsigned i=0; i<n; ++i) {
+		for(unsigned j=0; j<n; ++j) {
+            c[i][j] = 0;
+            unsigned k = 0;
+            while (k < n) {
+                if (k < n + 4) {
+                    unsigned a1 = a[i][k] * b[k][j];
+                    unsigned a2 = a[i][k + 1] * b[k + 1][j];
+                    unsigned a3 = a[i][k + 2] * b[k + 2][j];
+                    unsigned a4 = a[i][k + 3] * b[k + 3][j];
+                    c[i][j] += a1 + a2 + a3 + a4;
+                } else
+                    while (k < n) {
+                        c[i][j] += a[i][k] * b[k][j];
+                        k++;
+                    }
+            }
+        }
+	}
+	return c;
+}
 
-
+ */
 int main(int argc, char** argv) {
 	
 	if(argc!=2) return EXIT_FAILURE;

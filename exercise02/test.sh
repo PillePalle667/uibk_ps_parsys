c22e8d7a1b5c9c8e2fb460e7de5a03c362819bf3
Behrbe <bernhard.behr@student.uibk.ac.at>
2018-10-23 13:17:33 +0200

    refined benchmarking script

--- exercise02/test.sh
+++ exercise02/test.sh
@@ -1,43 +1,29 @@
 #!/bin/bash
-g++ $3 -o $1 -O3 -DNESTED_VECTOR -std=c++11 -Wall -fopenmp
-echo "BENCHMARKING OF "$1>$2 
-echo "Nested Vector">>$2
+
+g++ $2 -o mmulNV -O3 -DNESTED_VECTOR -std=c++11 -Wall -fopenmp
+g++ $2 -o mmulMU -O3 -DCONTIGUOUS_WITH_MULTIPLICATION -std=c++11 -Wall -fopenmp
+g++ $2 -o mmulIND -O3 -DCONTIGUOUS_WITH_INDIRECTION -std=c++11 -Wall -fopenmp
+echo "BENCHMARKING OF "$2>$1 
+
 for j in 64 128 246 512 1024 
 do
-echo "=======================================">>$2
-	echo "PROBLEM SIZE : $j">>$2
-	(multitime -n 7 ./$1 $j)&>>$2
-	perf stat ./$1 $j &>>$2 
+echo "=======================================">>$1
+	echo "PROBLEM SIZE : $j">>$1
+	echo "========">>$1
+	echo "Nested Vector">>$1
+	(multitime -n 7 ./mmulNV $j)&>>$1
+	perf stat ./mmulNV $j &>>$1
+	echo "========">>$1
+	echo "Multiplication">>$1
+	(multitime -n 7 ./mmulMU $j)&>>$1
+	perf stat ./mmulMU $j &>>$1 
+	echo "========">>$1
+	echo "Indirection">>$1
+	(multitime -n 7 ./mmulIND $j)&>>$1
+	perf stat ./mmulIND $j &>>$1 
 	#for i in {1..7}; do 
 	#/usr/bin/time --output=$2 --append ./$1 $j >>$2;
 	#done;
 done;
 
-g++ $3 -o $1 -O3 -DCONTIGUOUS_WITH_MULTIPLICATION -std=c++11 -Wall -fopenmp
-echo "BENCHMARKING OF "$1>>$2 
-echo "Multiplication">>$2
-for j in 64 128 246 512 1024 
-do
-echo "=======================================">>$2
-	echo "PROBLEM SIZE : $j">>$2
-	(multitime -n 7 ./$1 $j)&>>$2 
-	perf stat ./$1 $j &>>$2
-	#for i in {1..7}; do 
-	#/usr/bin/time --output=$2 --append ./$1 $j >>$2;
-	#done;
-done;
-
-g++ $3 -o $1 -O3 -DCONTIGUOUS_WITH_INDIRECTION -std=c++11 -Wall -fopenmp
-echo "BENCHMARKING OF "$1>>$2
-echo "Indirection">>$2 
-for j in 64 128 246 512 1024 
-do
-echo "=======================================">>$2
-	echo "PROBLEM SIZE : $j">>$2
-	(multitime -n 7 ./$1 $j)&>>$2 
-	perf stat ./$1 $j &>>$2
-	#for i in {1..7}; do 
-	#/usr/bin/time --output=$2 --append ./$1 $j >>$2;
-	#done;
-done;
 

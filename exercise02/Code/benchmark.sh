a037c70d6c1cbefffb2cf353a1204512d902b20a
KarenErrou <43989013+KarenErrou@users.noreply.github.com>
2018-10-24 12:47:11 +0200

    Update benchmark.sh

--- exercise02/Code/benchmark.sh
+++ exercise02/Code/benchmark.sh
@@ -9,19 +9,19 @@ for j in 64 128 246 512 1024
 do
 echo "=======================================">>$1
 	echo "PROBLEM SIZE : $j">>$1
+	
 	echo "========">>$1
 	echo "Nested Vector">>$1
 	(multitime -n 7 ./mmulNV $j)&>>$1
 	perf stat ./mmulNV $j &>>$1
+	
 	echo "========">>$1
 	echo "Multiplication">>$1
 	(multitime -n 7 ./mmulMU $j)&>>$1
 	perf stat ./mmulMU $j &>>$1 
+	
 	echo "========">>$1
 	echo "Indirection">>$1
 	(multitime -n 7 ./mmulIND $j)&>>$1
 	perf stat ./mmulIND $j &>>$1 
-	#for i in {1..7}; do 
-	#/usr/bin/time --output=$2 --append ./$1 $j >>$2;
-	#done;
 done;
